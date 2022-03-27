1afcba3521501ef0f8fd440af823e6455b3e5dd2
PedroJardim97 <85594972+PedroJardim97@users.noreply.github.com>
2022-03-27 02:53:58 -0300

    Bug de Address

--- relations/routes/api.php
+++ relations/routes/api.php
@@ -2,18 +2,29 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AddressController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//criar usuario
+Route::post('/users', [UserController::class, 'createUser']);
+
+// listar usuario
+Route::get('/users', [UserController::class, 'index']);
+
+//buscar por 1 ususario pelo id
+Route::get('/users/{id}', [UserController::class, 'findOne']);
+
+
+
+
+Route::get('/addresses',[AddressController::class, 'index']);
+
+//criar endereço
+Route::post('/addresses', [AddressController::class, 'createAddress']);
+
+//buscar 1 enderepo por id
+Route::get('/addresses/{id}', [AddressController::class, 'findOne']);
+
+
+
